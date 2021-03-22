3ee70a79733ca7958c483254e3e090527a406169
Lavinia Ma'afu <maaful0471@masseyhigh.school.nz>
2021-03-22 20:31:36 +0000

    game summary

--- 07_Game_Summary.py
+++ 07_Game_Summary.py
@@ -11,9 +11,28 @@ for item in range(0, 5):
 
   if results == "lost":
     rounds_lost += 1
-    elif results == "tie":
-      rounds-drawn += 1
 
-      game_summary.append(outcome)
+  elif results == "tie":
+    rounds_drawn += 1
+    
+  
+
+  game_summary.append(outcome)
 
 rounds_won = rounds_played - rounds_lost - rounds_drawn
+
+#  **** Calculate Game Stats *****
+percent_win = rounds_won / rounds_played * 100
+percent_lose = rounds_lost / rounds_played * 100
+percent_tie = rounds_drawn / rounds_played * 100
+
+print()
+print("***** Game History *******")
+for game in game_summary:
+  print(game)
+
+print()
+
+# display game stats with % values to the nearest whole number 
+print("***** Game Statistics *******")
+print("Win: {}, ({:.0f}%)\nLoss: {}, ({:.0f}%)\nTie: {}, ({:.0f}&)".format(rounds_won, percent_win, rounds_lost, percent_lose, rounds_drawn, percent_tie))
